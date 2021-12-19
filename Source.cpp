f4ea2df5e575fde302ec9ba195a4768ab94dface
Mr-Gradus <rybin.dmitriy@gmail.com>
2021-12-19 18:43:43 +0500

    task_1 DONE
    Первое задание сделал

--- Source.cpp
+++ Source.cpp
@@ -13,39 +13,72 @@ private:
 	int m_month;
 	int m_year;
 public:
-	Date() {}
+	Date (int day, int month, int year) : m_day(day), m_month(month), m_year(year) 
+	{
+		cout << "ctor" << endl;
+	}
 
-	int day(int d)
+	~Date()
+	{
+		cout << "DESTRUCOR" << endl;
+	}
+
+	int getDay() const
 	{
 		return m_day;
 	}
 
-	int month(int m)
+	int getMonth() const
 	{
 		return m_month;
 	}
 	
-	int year(int y)
+	int getYear() const
 	{
 		return m_year;
 	}
 	friend ostream& operator<<(ostream& out, const Date& oDate);
+
+	void print()
+	{
+		cout << "today!!!" << endl;
+	}
+
 };
 
 ostream& operator<<(ostream& out, const Date& oDate)
 {
-	out << &oDate.m_day << endl;
-	out << &oDate.m_month << endl;
-	out << &oDate.m_year << endl;
+	out << oDate.m_day << "." << oDate.m_month << "." << oDate.m_year << endl;
 	return out;
 }
+
+
 
 int main()
 {
 	cout << "#################### TASK_1 ####################" << endl << endl;
 
-	Date today;
-	cout << today.day(25);
+	unique_ptr<Date> today = make_unique<Date>(19, 12, 2021);
+	
+	cout << today->getDay() << "." << today->getMonth() << "." << today->getYear() << endl;
+	
+	cout << *today;
+	
+	
+	unique_ptr<Date> date = move(today);
+	bool NotEmpty = (bool)today;
+	//cout << boolalpha << NotEmpty << endl;
+	if (today)
+		cout << "ptr today not null" << endl;
+	else
+		cout << "ptr today is null" << endl;
+	
 
+	bool notEmpty = (bool)date;
+	if (date)
+		cout << "ptr date not null" << endl;
+	else
+		cout << "ptr date is null" << endl;
 	return 0;
+
 }
