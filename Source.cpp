b263ef64c3dbcc489b167540716605009635bb7b
Mr-Gradus <rybin.dmitriy@gmail.com>
2021-12-21 01:37:56 +0500

    task 2 done

--- Source.cpp
+++ Source.cpp
@@ -18,6 +18,7 @@ public:
 
 	~Date() {}
 	
+	void Compare(Date& d1, Date& d2);
 
 	int getDay() const
 	{
@@ -33,6 +34,9 @@ public:
 	{
 		return m_year;
 	}
+	
+	void SwapDate(Date& d1, Date& d2);
+
 	friend ostream& operator<<(ostream& out, const Date& oDate);
 
 };
@@ -45,71 +49,84 @@ ostream& operator<<(ostream& out, const Date& oDate)
 
 //#################### TASK_2 ####################
 
+		
+void Date::Compare(Date& d1 , Date& d2)
+{
+		if (d1.getYear() > d2.getYear())
+		{
+			cout << d1;
+		}
+		else if (d1.getYear() < d2.getYear())
+		{
+			cout << d2;
+		}
+		else {
+			if (d1.getMonth() > d2.getMonth())
+			{
+				cout << d1;
+			}
+			else if (d1.getMonth() < d2.getMonth())
+			{
+				cout << d2;
+			}
+			else
+			{
+				if (d1.getDay() > d2.getDay())
+				{
+					cout << d1;
+				}
+				else
+				{
+					cout << d2;
+				}
+
+			}
+		}
+	}
+
+void Date::SwapDate(Date& d1, Date& d2)
+{
+	swap(d1, d2);
+}
 
 
 int main()
 {
+	{
 
+		cout << "#################### TASK_1 ####################" << endl << endl;
 
-	cout << "#################### TASK_1 ####################" << endl << endl;
+		unique_ptr<Date> today = make_unique<Date>(19, 12, 2021);
 
-	unique_ptr<Date> today = make_unique<Date>(19, 12, 2021);
+		cout << today->getDay() << "." << today->getMonth() << "." << today->getYear() << endl;
 
-	cout << today->getDay() << "." << today->getMonth() << "." << today->getYear() << endl;
+		cout << *today;
+		
+		unique_ptr<Date> date = move(today);
+		bool NotEmpty = (bool)today;
+		if (today)
+			cout << "ptr today not null" << endl;
+		else
+			cout << "ptr today is null" << endl;
 
-	cout << *today;
+		bool notEmpty = (bool)date;
+		if (date)
+			cout << "ptr date not null" << endl;
+		else
+			cout << "ptr date is null" << endl;
 
-
-	unique_ptr<Date> date = move(today);
-	bool NotEmpty = (bool)today;
-	//cout << boolalpha << NotEmpty << endl;
-	if (today)
-		cout << "ptr today not null" << endl;
-	else
-		cout << "ptr today is null" << endl;
-
-	bool notEmpty = (bool)date;
-	if (date)
-		cout << "ptr date not null" << endl;
-	else
-		cout << "ptr date is null" << endl;
-
-
+	}
 	cout << "#################### TASK_2 ####################" << endl << endl;
 
-	unique_ptr<Date> date1 = make_unique<Date>(13, 8, 2021);
-	unique_ptr<Date> date2 = make_unique<Date>(9, 12, 2021);
-
-	if (date1->getYear() > date2->getYear())
-	{
-		cout << *date1 << "later date" << endl;
-	}
-	else if (date1->getYear() < date2->getYear())
-	{
-		cout << *date2 << "later date" << endl;
-	}
-	else {
-		if (date1->getMonth() > date2->getMonth())
-		{
-			cout << *date1 << "later date" << endl;
-		}
-		else if (date1->getMonth() < date2->getMonth())
-		{
-			cout << *date2 << "later date" << endl;
-		}
-		else
-		{
-			if (date1->getDay() > date2->getDay())
-			{
-				cout << *date1 << "later date" << endl;
-			}
-			else
-			{
-				cout << *date2 << "later date" << endl;
-			}
-
-		}
-	}
+	unique_ptr<Date> date1 = make_unique<Date>(6, 6, 1906);
+	unique_ptr<Date> date2 = make_unique<Date>(21, 12, 2021);
+	cout << "Later date is: ";
+	date2->Compare(*date1, *date2);
+	cout << "ptr date1: " << *date1;
+	cout << "ptr date2: " << *date2;
+	date1->SwapDate(*date1, *date2);
+	cout << "ptr date1: " << *date1;
+	cout << "ptr date2: " << *date2;
 	return 0;
 
 }
